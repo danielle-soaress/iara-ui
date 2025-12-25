97d74736c9164ecd7f1d8bf3412bf44a42baf5d8
danielle <silvasoaresdanielle2@gmail.com>
2025-12-25 19:27:08 -0300

    feat: app_image and app_vector file + some files & vectors

---
+++ iara/lib/core/configs/assets/app_images.dart
@@ -0,0 +1,8 @@
+class AppImages {
+  static const String basePath = 'assets/images/';
+
+  static const String backgroundBlur = '${basePath}background_blur.png';
+  static const String textLogo = '${basePath}AILA_text.png';
+  static const String smilingAILA = '${basePath}expr_AILA_smiling.png';
+  static const String happyAILA = '${basePath}expr_AILA_happy_face.png';
+}
---
+++ iara/lib/core/configs/assets/app_vectors.dart
@@ -0,0 +1,10 @@
+class AppVectors {
+  static const String basePath = 'assets/vectors/';
+  static const String format = '.svg';
+
+  static const String splash = '${basePath}splash_vector$format';
+
+  static const String academic = '${basePath}academic$format';
+  static const String conversation = '${basePath}conversation$format';
+  static const String world = '${basePath}world$format';
+}
